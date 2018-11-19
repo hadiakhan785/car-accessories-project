8442328c5190ed81759c1adbccd286c154b00599
hadiakhan785 <hadiakhan785@gmail.com>
2018-11-19 18:10:09 +0500

    fifth commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function collapseMenu() {
+    if (document.querySelector('.collapse-nav').style.display == "none") {
+        document.querySelector('.collapse-nav').style.display = "block";
+    } else {
+        document.querySelector('.collapse-nav').style.display = "none";
+    }
+}
