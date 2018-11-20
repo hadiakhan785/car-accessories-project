47ff4c385caff91d1654b7c9cca882b302dba3cb
hadiakhan785 <hadiakhan785@gmail.com>
2018-11-20 17:55:09 +0500

    seventh commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 function init() {
+    // event listener
     document.querySelector('article').addEventListener('click', () => {
+        if (document.querySelector('.collapse-nav').style.visibility == 'visible') {
+            document.querySelector('.collapse-nav').style.visibility = 'hidden';
+        }
+    });
+
+    document.querySelector('footer').addEventListener('click', () => {
+        if (document.querySelector('.collapse-nav').style.visibility == 'visible') {
+            document.querySelector('.collapse-nav').style.visibility = 'hidden';
+        }
+    });
+
+    document.querySelector('.collapse-nav').addEventListener('click', () => {
         if (document.querySelector('.collapse-nav').style.visibility == 'visible') {
             document.querySelector('.collapse-nav').style.visibility = 'hidden';
         }
@@ -8,10 +21,16 @@ function init() {
 
 function collapseMenu() {
     if (document.querySelector('.collapse-nav').style.visibility == 'hidden') {
+        document.querySelector('.collapse-nav').style.transition = '800ms';        
         document.querySelector('.collapse-nav').style.visibility = 'visible';
+        document.querySelector('.collapse-nav').classList.remove('fadeOutLeft');
+        document.querySelector('.collapse-nav').classList.add('fadeInLeft');
 
     } else {
+        document.querySelector('.collapse-nav').classList.remove('fadeInLeft');
+        document.querySelector('.collapse-nav').classList.add('fadeOutLeft');
         document.querySelector('.collapse-nav').style.visibility = 'hidden';
+
 
     }
 }
