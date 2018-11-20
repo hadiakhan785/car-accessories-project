d073a772130cb240c823f6d20c2b3d71cd6a2e21
hadiakhan785 <hadiakhan785@gmail.com>
2018-11-20 16:44:31 +0500

    sixth commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+function init() {
+    document.querySelector('article').addEventListener('click', () => {
+        if (document.querySelector('.collapse-nav').style.visibility == 'visible') {
+            document.querySelector('.collapse-nav').style.visibility = 'hidden';
+        }
+    });
+}
+
 function collapseMenu() {
-    if (document.querySelector('.collapse-nav').style.display == "none") {
-        document.querySelector('.collapse-nav').style.display = "block";
+    if (document.querySelector('.collapse-nav').style.visibility == 'hidden') {
+        document.querySelector('.collapse-nav').style.visibility = 'visible';
+
     } else {
-        document.querySelector('.collapse-nav').style.display = "none";
+        document.querySelector('.collapse-nav').style.visibility = 'hidden';
+
     }
 }
